5930f5fd2959d8e11709a8c79ab878b5c2102adc
Peng Lyu <penn.lv@gmail.com>
2016-12-01 10:54:57 -0800

    add decreaseIndentPattern (#37)
    * add decreaseIndentPattern
    
    * Update launch.json

--- ruby.js
+++ ruby.js
@@ -10,7 +10,8 @@ let AutoCorrect = require('./format/RuboCop');
 
 const langConfig = {
 	indentationRules: {
-		increaseIndentPattern: /^\s*((begin|class|def|else|elsif|ensure|for|if|module|rescue|unless|until|when|while)|(.*\sdo\b))\b[^\{;]*$/
+		increaseIndentPattern: /^\s*((begin|class|def|else|elsif|ensure|for|if|module|rescue|unless|until|when|while)|(.*\sdo\b))\b[^\{;]*$/,
+		decreaseIndentPattern: /^\\s*([}\\]](,?\\s*(#|$)|\\.[a-zA-Z_]\\w*\\b)|(end|rescue|ensure|else|elsif|when)\\b)/
 	},
 	wordPattern: /(-?\d+(?:\.\d+))|(:?[A-Za-z][^-`~@#%^&()=+[{}|;:'",<>/.*\]\s\\!?]*[!?]?)/
 };
