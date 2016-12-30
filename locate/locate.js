40eaa99ac8af4dcf193d629f451cc8d96745c67c
Yuya Tanaka <yuya.presto@gmail.com>
2016-12-31 04:55:48 +0900

    Implement WorkspaceSymbolProvider

--- locate/locate.js
+++ locate/locate.js
@@ -7,29 +7,54 @@ const _ = require('lodash');
 
 const DECLARATION_TYPES = ['class', 'module', 'method', 'classMethod'];
 
-function flatten(locateInfo, file, parent) {
+function flatten(locateInfo, file, containerName = '') {
 	return _.flatMap(locateInfo, (symbols, type) => {
 		if (!_.includes(DECLARATION_TYPES, type)) {
 			// Skip top-level include or posn property etc.
 			return [];
 		}
 		return _.flatMap(symbols, (inner, name) => {
-			const sep = { method: '#', classMethod: '.' }[type] || '::';
 			const posn = inner.posn || { line: 0, char: 0 };
-			const fullName = parent ? `${parent.fullName}${sep}${name}` : name;
+			// TODO: parse name with multiple segments, e.g. File.read or ActiveRecord::Base, if necessary.
 			const symbolInfo = {
 				name: name,
 				type: type,
 				file: file,
 				line: posn.line,
 				char: posn.char,
-				parent: parent,
-				fullName: fullName
+				containerName: containerName || ''
 			};
 			_.extend(symbolInfo, _.omit(inner, DECLARATION_TYPES));
-			return [symbolInfo].concat(flatten(inner, file, symbolInfo));
+			const sep = { method: '#', classMethod: '.' }[type] || '::';
+			const fullName = containerName ? `${containerName}${sep}${name}` : name;
+			return [symbolInfo].concat(flatten(inner, file, fullName));
 		});
 	});
+}
+function camelCaseRegExp(query) {
+	const escaped = _.escapeRegExp(query)
+	const prefix = escaped.charAt(0);
+	return new RegExp(
+		`[${prefix.toLowerCase()}${prefix.toUpperCase()}]` +
+		escaped.slice(1).replace(/[A-Z]|([a-z])/g, (char, lower) => {
+			if (lower) return `[${char.toUpperCase()}${char}]`;
+			const lowered = char.toLowerCase()
+			return `.*(?:${char}|_${lowered})`;
+		})
+	);
+}
+function filter(symbols, query, stringProvider) {
+	// TODO: Ask MS to expose or separate matchesFuzzy method.
+	// https://github.com/Microsoft/vscode/blob/a1d3c8a3006d0a3d68495122ea09a2a37bca69db/src/vs/base/common/filters.ts
+	const isLowerCase = (query.toLowerCase() === query)
+	const exact = new RegExp('^' + _.escapeRegExp(query) + '$', 'i');
+	const prefix = new RegExp('^' + _.escapeRegExp(query), 'i');
+	const substring = new RegExp(_.escapeRegExp(query), isLowerCase ? 'i' : '');
+	const camelCase = camelCaseRegExp(query);
+	return _([exact, prefix, substring, camelCase])
+		.flatMap(regexp => symbols.filter(symbolInfo => regexp.test(stringProvider(symbolInfo))))
+		.uniq()
+		.value();
 }
 module.exports = class Locate {
 	constructor(root, settings) {
@@ -57,6 +82,18 @@ module.exports = class Locate {
 			.filter(symbol => regexp.test(symbol.name))
 			.map(_.clone)
 			.value();
+	}
+	query(query) {
+		const match = query.match(/^(?:(.*)[.#])?([^.#]*)$/);
+		const containerQuery = match[1];
+		const nameQuery = match[2];
+		if (!nameQuery) return [];
+
+		const symbols = _(this.tree).values().flatten().value();
+		const matchedSymbols = filter(symbols, nameQuery, symbolInfo => symbolInfo.name);
+		if (!containerQuery) return matchedSymbols;
+
+		return filter(matchedSymbols, containerQuery, symbolInfo => symbolInfo.containerName);
 	}
 	rm(absPath) {
 		if (absPath in this.tree) delete this.tree[absPath];
