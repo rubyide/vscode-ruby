353e3629d9f73a86a2449c336a7e159d1eb953e3
TeeSeal <alex.theoddor@gmail.com>
2017-12-15 13:23:57 +0200

    Single quote and tabs

--- src/ruby.ts
+++ src/ruby.ts
@@ -13,13 +13,13 @@ import { registerTaskProvider } from './task/rake';
 export function activate(context: ExtensionContext) {
 	const subs = context.subscriptions;
 	// register language config
-	vscode.languages.setLanguageConfiguration("ruby", {
-    indentationRules: {
-      increaseIndentPattern: /^\s*((begin|class|((private|protected)\s+)?def|else|elsif|ensure|for|if|module|rescue|unless|until|when|while|case)|([^#]*\sdo\b)|([^#]*=\s*(case|if|unless)))\b([^\{;]|("|'|\/).+\4)*$/,
-      decreaseIndentPattern: /^\s*([}\]]([,)]?\s*(#|$)|\.[a-zA-Z_]\w*\b)|(end|rescue|ensure|else|elsif|when)\b)/
-    },
-    wordPattern: /(-?\d+(?:\.\d+))|(:?[A-Za-z][^-`~@#%^&()=+[{}|;:'",<>/.*\]\s\\!?]*[!?]?)/
-  })
+	vscode.languages.setLanguageConfiguration('ruby', {
+		indentationRules: {
+			increaseIndentPattern: /^\s*((begin|class|((private|protected)\s+)?def|else|elsif|ensure|for|if|module|rescue|unless|until|when|while|case)|([^#]*\sdo\b)|([^#]*=\s*(case|if|unless)))\b([^\{;]|("|'|\/).+\4)*$/,
+			decreaseIndentPattern: /^\s*([}\]]([,)]?\s*(#|$)|\.[a-zA-Z_]\w*\b)|(end|rescue|ensure|else|elsif|when)\b)/
+		},
+		wordPattern: /(-?\d+(?:\.\d+))|(:?[A-Za-z][^-`~@#%^&()=+[{}|;:'",<>/.*\]\s\\!?]*[!?]?)/
+	});
 
 	registerHighlightProvider(context);
 	registerLinters(context);
