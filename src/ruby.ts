c058e637b126696a4e334db47cbda242df1dcd85
Fred Snyder <fsnyder@castwide.com>
2018-03-08 10:43:39 -0500

    codeCompletion and intellisense provider settings

--- src/ruby.ts
+++ src/ruby.ts
@@ -25,9 +25,13 @@ export function activate(context: ExtensionContext) {
 
 	registerHighlightProvider(context);
 	registerLinters(context);
-	registerCompletionProvider(context);
+	if (vscode.workspace.getConfiguration('ruby').codeCompletion == 'rcodetools') {
+		registerCompletionProvider(context);
+	}
 	registerFormatter(context);
-	registerIntellisenseProvider(context);
+	if (vscode.workspace.getConfiguration('ruby').intellisense == 'rubyLocate') {
+		registerIntellisenseProvider(context);
+	}
 	registerTaskProvider(context);
 	utils.loadEnv();
 }
