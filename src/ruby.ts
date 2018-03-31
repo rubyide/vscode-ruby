75106ee65cb2c7f21ba2674d2adffa30c01fc4bd
Stafford Brunk <wingrunr21@gmail.com>
2018-03-31 15:31:18 -0600

    Minor cleanup of ruby.ts
    This isn't fully compliant with the tslint/prettier config but is enough for now

--- src/ruby.ts
+++ src/ruby.ts
@@ -1,11 +1,10 @@
-"use strict";
+'use strict';
 
-import * as vscode from 'vscode';
-import { Location, ExtensionContext, Position } from 'vscode';
-import * as Locate from './locate/locate';
 import * as path from 'path';
-import * as utils from './utils';
+import { ExtensionContext, languages, Location, Position } from 'vscode';
+import * as Locate from './locate/locate';
 import { registerTaskProvider } from './task/rake';
+import * as utils from './utils';
 
 import { registerCompletionProvider } from './providers/completion';
 import { registerFormatter } from './providers/formatter';
@@ -15,20 +14,23 @@ import { registerLinters } from './providers/linters';
 
 export function activate(context: ExtensionContext) {
 	const subs = context.subscriptions;
+
 	// register language config
-	vscode.languages.setLanguageConfiguration('ruby', {
+	languages.setLanguageConfiguration('ruby', {
 		indentationRules: {
 			increaseIndentPattern: /^(\s*(module|class|((private|protected)\s+)?def|unless|if|else|elsif|case|when|begin|rescue|ensure|for|while|until|(?=.*?\b(do|begin|case|if|unless)\b)("(\\.|[^\\"])*"|'(\\.|[^\\'])*'|[^#"'])*(\s(do|begin|case)|[-+=&|*/~%^<>~]\s*(if|unless)))\b(?![^;]*;.*?\bend\b)|("(\\.|[^\\"])*"|'(\\.|[^\\'])*'|[^#"'])*(\((?![^\)]*\))|\{(?![^\}]*\})|\[(?![^\]]*\]))).*$/,
-			decreaseIndentPattern: /^\s*([}\]]([,)]?\s*(#|$)|\.[a-zA-Z_]\w*\b)|(end|rescue|ensure|else|elsif|when)\b)/
+			decreaseIndentPattern: /^\s*([}\]]([,)]?\s*(#|$)|\.[a-zA-Z_]\w*\b)|(end|rescue|ensure|else|elsif|when)\b)/,
 		},
-		wordPattern: /(-?\d+(?:\.\d+))|(:?[A-Za-z][^-`~@#%^&()=+[{}|;:'",<>/.*\]\s\\!?]*[!?]?)/
+		wordPattern: /(-?\d+(?:\.\d+))|(:?[A-Za-z][^-`~@#%^&()=+[{}|;:'",<>/.*\]\s\\!?]*[!?]?)/,
 	});
 
+	// Register providers
 	registerHighlightProvider(context);
 	registerLinters(context);
 	registerCompletionProvider(context);
 	registerFormatter(context);
 	registerIntellisenseProvider(context);
 	registerTaskProvider(context);
+
 	utils.loadEnv();
 }
