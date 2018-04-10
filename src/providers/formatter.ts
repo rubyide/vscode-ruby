ab37cd2753a737f57ba2c72141217f017f4cb044
Jonathan Carter <joncart@outlook.com>
2018-04-10 09:03:15 -0700

    Restricting language services to local files

--- src/providers/formatter.ts
+++ src/providers/formatter.ts
@@ -1,6 +1,6 @@
-import { ExtensionContext } from 'vscode';
+import { DocumentSelector, ExtensionContext } from 'vscode';
 import { RubyDocumentFormattingEditProvider } from '../format/rubyFormat';
 
-export function registerFormatter(ctx: ExtensionContext) {
-	new RubyDocumentFormattingEditProvider().register(ctx);
+export function registerFormatter(ctx: ExtensionContext, documentSelector: DocumentSelector) {
+	new RubyDocumentFormattingEditProvider().register(ctx, documentSelector);
 }
