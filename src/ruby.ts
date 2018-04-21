7f5a388011baad1cf53ecb21f909a01d3d245fd1
Stafford Brunk <wingrunr21@gmail.com>
2018-04-21 14:35:02 -0600

    Start build-in language server if useLanguageServer is set

--- src/ruby.ts
+++ src/ruby.ts
@@ -1,6 +1,8 @@
-'use strict';
-
+/**
+ * vscode-ruby main
+ */
 import { ExtensionContext, languages, workspace } from 'vscode';
+import * as client from 'vscode-ruby-client';
 
 import * as utils from './utils';
 
@@ -12,19 +14,23 @@ import { registerIntellisenseProvider } from './providers/intellisense';
 import { registerLinters } from './providers/linters';
 import { registerTaskProvider } from './task/rake';
 
-const DOCUMENT_SELECTOR = [
+const DOCUMENT_SELECTOR: { language: string; scheme: string }[] = [
 	{ language: 'ruby', scheme: 'file' },
 	{ language: 'ruby', scheme: 'untitled' }
 ];
 
-export function activate(context: ExtensionContext) {
-	const subs = context.subscriptions;
-
+export function activate(context: ExtensionContext): void {
 	// register language config
 	languages.setLanguageConfiguration('ruby', languageConfiguration);
 
+	if (workspace.getConfiguration('ruby').useLanguageServer) {
+		client.activate(context);
+	} else {
+		// Register legacy providers
+		registerHighlightProvider(context, DOCUMENT_SELECTOR);
+	}
+
 	// Register providers
-	registerHighlightProvider(context, DOCUMENT_SELECTOR);
 	registerCompletionProvider(context, DOCUMENT_SELECTOR);
 	registerFormatter(context, DOCUMENT_SELECTOR);
 
@@ -35,4 +41,10 @@ export function activate(context: ExtensionContext) {
 	}
 
 	utils.loadEnv();
+}
+
+export function deactivate(): void {
+	client.deactivate();
+
+	return undefined;
 }
