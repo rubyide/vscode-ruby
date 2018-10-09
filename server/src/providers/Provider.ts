34df452058436e90ea510a5eba4570801ed7ce0c
Stafford Brunk <wingrunr21@gmail.com>
2018-10-09 16:45:54 -0600

    Remove static register function from Provider
    It isn't really serving a purpose

--- server/src/providers/Provider.ts
+++ server/src/providers/Provider.ts
@@ -5,10 +5,6 @@ export default abstract class Provider {
 	protected connection: IConnection;
 	protected forest: IForest;
 
-	static register(connection: IConnection, forest: IForest) {
-		throw new Error('Register is not implemented!');
-	}
-
 	constructor(connection: IConnection, forest: IForest) {
 		this.connection = connection;
 		this.forest = forest;
