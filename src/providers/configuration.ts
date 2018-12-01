90ca8994ee58ea63156f4ae1a0734b491f71af4f
Yisrael <israelw1992@gmail.com>
2018-12-01 22:41:09 +0200

    Use DebugConfigurationProvider
    Remove configurations from package.json and provide them at run time
    with a provider

---
+++ src/providers/configuration.ts
@@ -0,0 +1,71 @@
+import * as vscode from 'vscode';
+
+export function registerConfigurationProvider(): void {
+	vscode.debug.registerDebugConfigurationProvider("Ruby", new RubyConfigurationProvider());
+}
+
+class RubyConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+	public provideDebugConfigurations(folder: vscode.WorkspaceFolder, token: vscode.CancellationToken): vscode.DebugConfiguration[] {
+		return rubyConfigurations;
+	}
+}
+
+const rubyConfigurations: vscode.DebugConfiguration[] = [
+	{
+		"name": "Debug Local File",
+		"type": "Ruby",
+		"request": "launch",
+		"cwd": "${workspaceRoot}",
+		"program": "${workspaceRoot}/main.rb"
+	},
+	{
+		"name": "Listen for rdebug-ide",
+		"type": "Ruby",
+		"request": "attach",
+		"cwd": "${workspaceRoot}",
+		"remoteHost": "127.0.0.1",
+		"remotePort": "1234",
+		"remoteWorkspaceRoot": "${workspaceRoot}"
+	},
+	{
+		"name": "Rails server",
+		"type": "Ruby",
+		"request": "launch",
+		"cwd": "${workspaceRoot}",
+		"program": "${workspaceRoot}/bin/rails",
+		"args": [
+			"server"
+		]
+	},
+	{
+		"name": "RSpec - all",
+		"type": "Ruby",
+		"request": "launch",
+		"cwd": "${workspaceRoot}",
+		"program": "${workspaceRoot}/bin/rspec",
+		"args": [
+			"-I",
+			"${workspaceRoot}"
+		]
+	},
+	{
+		"name": "RSpec - active spec file only",
+		"type": "Ruby",
+		"request": "launch",
+		"cwd": "${workspaceRoot}",
+		"program": "${workspaceRoot}/bin/rspec",
+		"args": [
+			"-I",
+			"${workspaceRoot}",
+			"${file}"
+		]
+	},
+	{
+		"name": "Cucumber",
+		"type": "Ruby",
+		"request": "launch",
+		"cwd": "${workspaceRoot}",
+		"program": "${workspaceRoot}/bin/cucumber"
+	}
+]
