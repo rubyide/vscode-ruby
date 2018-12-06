4e72ab2d37bdf3a8b147471d2b38ae9343d26ceb
Ted <ted@kcd.co.kr>
2018-12-06 11:01:12 +0900

    fixup! Respect ruby.pathToBundler for RuboCop executable

--- src/format/RuboCop.ts
+++ src/format/RuboCop.ts
@@ -42,7 +42,8 @@ export class AutoCorrect {
 
 		const ext: string = process.platform === 'win32' ? '.bat' : '';
 		if (vscode.workspace.getConfiguration('ruby').useBundler) {
-			let pathToBundler = vscode.workspace.getConfiguration('ruby').pathToBundler || 'bundle';
+			const pathToBundler: string =
+				vscode.workspace.getConfiguration('ruby').pathToBundler || 'bundle';
 			return [`${pathToBundler}${ext}`, 'exec', 'rubocop'];
 		}
 
