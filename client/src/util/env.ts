3305dd14f71548444f51b0037705fdecb7bab5d4
Stafford Brunk <wingrunr21@gmail.com>
2019-02-24 13:31:47 -0700

    Implement more robust env variable processing

--- client/src/util/env.ts
+++ client/src/util/env.ts
@@ -33,6 +33,35 @@ function getShim(): string {
 	return shimPath;
 }
 
+// Based on the dotenv parse function:
+// https://github.com/motdotla/dotenv/blob/master/lib/main.js#L32
+// modified to not have to deal with Buffers and to handle stuff
+// like export and declare -x at the start of the line
+function processExportLine(line: string): string[] {
+	const result = [];
+	// matching "KEY' and 'VAL' in 'KEY=VAL' with support for arbitrary prefixes
+	const keyValueArr = line.match(/^(?:[\w-]*\s+)*([\w.-]+)\s*=\s*(.*)?\s*$/);
+	if (keyValueArr != null) {
+		const key = keyValueArr[1];
+
+		// default undefined or missing values to empty string
+		let value = keyValueArr[2] || '';
+
+		// expand newlines in quoted values
+		const len = value ? value.length : 0;
+		if (len > 0 && value.charAt(0) === '"' && value.charAt(len - 1) === '"') {
+			value = value.replace(/\\n/gm, '\n');
+		}
+
+		// remove any surrounding quotes and extra spaces
+		value = value.replace(/(^['"]|['"]$)/g, '').trim();
+
+		result.push(key, value);
+	}
+
+	return result;
+}
+
 const RUBY_ENVIRONMENT_VARIABLES = [
 	'PATH',
 	'RUBY_VERSION',
@@ -61,14 +90,15 @@ export interface IEnvironment {
 
 export function loadEnv(cwd: string): RubyEnvironment {
 	const shim: string = getShim();
+	const env: IEnvironment = {};
 	const { stdout, stderr } = execa.sync(shim, [], {
 		cwd,
 	});
 
 	console.error(stderr);
 
-	for (const envVar of stdout.split('\n')) {
-		const result: string[] = envVar.split('=', 2);
+	for (const line of stdout.split('\n')) {
+		const result: string[] = processExportLine(line);
 		if (RUBY_ENVIRONMENT_VARIABLES.indexOf(result[0]) >= 0) {
 			env[result[0]] = result[1];
 		}
