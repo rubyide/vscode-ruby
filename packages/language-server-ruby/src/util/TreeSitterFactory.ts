93475107e5211b835d7452204aa9c1dd90fcf7c7
Stafford Brunk <wingrunr21@gmail.com>
2019-08-09 09:39:27 -0600

    Convert TreeSitterFactory to be an object vs a class

--- packages/language-server-ruby/src/util/TreeSitterFactory.ts
+++ packages/language-server-ruby/src/util/TreeSitterFactory.ts
@@ -1,20 +1,21 @@
 import path from 'path';
 import Parser from 'web-tree-sitter';
 
-const TREE_SITTER_RUBY_WASM = path.join(__dirname, '..', 'tree-sitter-ruby.wasm');
+const TREE_SITTER_RUBY_WASM = path.resolve(__dirname, 'tree-sitter-ruby.wasm');
 
-export default class TreeSitterFactory {
-	static language: any;
+const TreeSitterFactory = {
+	language: null,
 
-	static async initalize() {
+	async initalize(): Promise<void> {
 		await Parser.init();
 		console.debug(`Loading Ruby tree-sitter syntax from ${TREE_SITTER_RUBY_WASM}`);
 		this.language = await Parser.Language.load(TREE_SITTER_RUBY_WASM);
-	}
+	},
 
-	static build() {
+	build(): Parser {
 		const parser = new Parser();
 		parser.setLanguage(this.language);
 		return parser;
-	}
-}
+	},
+};
+export default TreeSitterFactory;
