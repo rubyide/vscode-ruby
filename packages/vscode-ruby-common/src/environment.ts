847e602f70207ec816608ddf9da46805c1f41a9a
Stafford Brunk <wingrunr21@gmail.com>
2019-10-13 08:42:09 -0600

    Add vscode-ruby-common package. Implement/test env detection there

---
+++ packages/vscode-ruby-common/src/environment.ts
@@ -0,0 +1,177 @@
+import spawn from 'cross-spawn';
+import fs from 'fs';
+import path from 'path';
+
+const SHIM_DIR = path.resolve(__dirname, 'shims');
+
+function isWindows(): boolean {
+	return process.platform === 'win32';
+}
+
+function defaultShell(): string {
+	if (isWindows()) {
+		return process.env.COMSPEC || 'cmd.exe';
+	}
+
+	if (!process.env.SHELL) {
+		return process.platform === 'darwin' ? '/bin/bash' : '/bin/sh';
+	}
+
+	return process.env.SHELL;
+}
+
+function shimExtension(shell: string): string {
+	let extension = 'sh';
+	if (shell.indexOf('fish') >= 0) extension = 'fish';
+	else if (isWindows()) extension = 'cmd';
+	return extension;
+}
+
+function getTemplate(shell: string): string {
+	let template;
+	if (isWindows()) {
+		template = 'SET';
+	} else if (shell.indexOf('fish') >= 0) {
+		template = `#!${shell}
+for name in (set -nx)
+	if string match --quiet '*PATH' $name
+		echo $name=(string join : -- $$name)
+	else
+		echo $name="$$name"
+	end
+end`;
+	} else {
+		template = `#!${shell} -i\nexport -p`;
+	}
+
+	return template;
+}
+
+function mkShim(shell: string, shimPath: string): boolean {
+	const template = getTemplate(shell);
+	let result = false;
+
+	try {
+		fs.writeFileSync(shimPath, template);
+		fs.chmodSync(shimPath, 0o744);
+		result = true;
+	} catch (e) {
+		console.error(e);
+	}
+
+	return result;
+}
+
+function getShim(shell: string, shimDir: string): string {
+	let shellName: string = shell.replace(new RegExp(path.sep, 'g'), '.');
+	if (shellName[0] === '.') shellName = shellName.slice(1);
+	const shimPath = path.join(shimDir, `env.${shellName}.${shimExtension(shellName)}`);
+	if (!fs.existsSync(shimDir)) {
+		fs.mkdirSync(shimDir);
+	}
+	if (!fs.existsSync(shimPath)) {
+		mkShim(shell, shimPath);
+	}
+
+	return shimPath;
+}
+
+// Based on the dotenv parse function:
+// https://github.com/motdotla/dotenv/blob/master/lib/main.js#L32
+// modified to not have to deal with Buffers and to handle stuff
+// like export and declare -x at the start of the line
+function processExportLine(line: string): string[] {
+	const result = [];
+	// matching "KEY' and 'VAL' in 'KEY=VAL' with support for arbitrary prefixes
+	const keyValueArr = line.match(/^(?:[\w-]*\s+)*([\w.-]+)\s*=\s*(.*)?\s*$/);
+	if (keyValueArr != null) {
+		const key = keyValueArr[1];
+
+		// default undefined or missing values to empty string
+		let value = keyValueArr[2] || '';
+
+		// expand newlines in quoted values
+		const len = value ? value.length : 0;
+		if (len > 0 && value.charAt(0) === '"' && value.charAt(len - 1) === '"') {
+			value = value.replace(/\\n/gm, '\n');
+		}
+
+		// remove any surrounding quotes and extra spaces
+		value = value.replace(/(^['"]|['"]$)/g, '').trim();
+
+		result.push(key, value);
+	}
+
+	return result;
+}
+
+function processEnvironment(output: string): IEnvironment {
+	const env: IEnvironment = {};
+	for (const line of output.split('\n')) {
+		const result: string[] = processExportLine(line);
+		const name = result[0];
+		if (RUBY_ENVIRONMENT_VARIABLES.indexOf(name) >= 0) {
+			env[name] = result[1];
+		}
+	}
+
+	return env;
+}
+
+// Whitelist environment variables to pass on
+// Don't want to pull in potentially secret variables
+const RUBY_ENVIRONMENT_VARIABLES = [
+	'PATH',
+	'Path', // Windows
+	'PATHEXT', // Windows
+	'RUBY_VERSION',
+	'RUBY_ROOT',
+	'RUBY_ENGINE',
+	'RUBYOPT',
+	'GEM_HOME',
+	'GEM_PATH',
+	'GEM_ROOT',
+	'HOME',
+	'RUBOCOP_OPTS',
+	'LANG',
+];
+
+export interface IEnvironment {
+	[key: string]: string;
+}
+
+export interface RubyEnvironment extends IEnvironment {
+	PATH: string;
+	Path?: string; // Windows
+	PATHEXT?: string; // Windows
+	RUBY_VERSION: string;
+	RUBY_ROOT: string;
+	RUBY_ENGINE: string;
+	RUBYOPT: string;
+	GEM_HOME: string;
+	GEM_PATH: string;
+	GEM_ROOT: string;
+	HOME: string;
+	RUBOCOP_OPTS: string;
+	LANG: string;
+}
+
+export interface LoadEnvOptions {
+	shell?: string;
+	shimDir?: string;
+}
+
+export function loadEnv(cwd: string, options?: LoadEnvOptions): IEnvironment {
+	const { shell = defaultShell(), shimDir = SHIM_DIR } = options;
+	const shim: string = getShim(shell, shimDir);
+
+	const { stdout, stderr, status } = spawn.sync(shim, [], {
+		cwd,
+	});
+
+	if (status !== 0) {
+		console.error(stderr.toString());
+	}
+
+	return processEnvironment(stdout.toString());
+}
---
+++ packages/vscode-ruby-common/test/environment.spec.ts
@@ -0,0 +1,214 @@
+import chai from 'chai';
+import path from 'path';
+import fs from 'fs';
+import sinon from 'sinon';
+import spawn from 'cross-spawn';
+import { SpawnSyncReturns } from 'child_process';
+import { loadEnv } from '../src/environment';
+
+// Setup for chai-fs
+chai.use(require('chai-fs'));
+const { expect } = chai;
+
+// Common directories
+const shimDir = path.resolve(__dirname, 'shims');
+const fixturesDir = path.resolve(__dirname, 'fixtures', 'environment');
+
+describe('environment', () => {
+	let spawnSyncStub;
+
+	before(() => {
+		delete process.env.SHELL;
+		spawnSyncStub = sinon.stub(spawn, 'sync').returns(spawnSyncReturnsFactory(''));
+	});
+
+	describe('#loadEnv', () => {
+		context('the default shell is used', () => {
+			context('the platform is Windows', () => {
+				before(() => {
+					mockPlatform('win32');
+					spawnSyncStub.returns(spawnSyncReturnsFactory(loadEnvironmentFixture('win32')));
+				});
+
+				after(() => {
+					unmockPlatform();
+				});
+
+				it('writes a shim to the file system', () => {
+					loadEnv(__dirname, { shimDir });
+					expect(path.join(shimDir, 'env.cmd.exe.cmd'))
+						.to.be.a.file(null)
+						.with.content('SET');
+				});
+
+				it('correctly loads the environment', () => {
+					const environment = loadEnv(__dirname, { shimDir });
+					expect(environment.PATHEXT).to.eq(
+						'.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.RB;.RBW'
+					);
+					// expect(environment.Path).to.eql(
+					// 	`C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Ruby26-x64\bin;C:\Users\someuser\AppData\Local\Microsoft\WindowsApps;;C:\Users\someuser\AppData\Local\Programs\Microsoft VS Code\bin`
+					// );
+				});
+			});
+
+			context('the platform is darwin', () => {
+				before(() => {
+					mockPlatform('darwin');
+				});
+
+				after(() => {
+					unmockPlatform();
+				});
+
+				it('writes a shim to the file system', () => {
+					loadEnv(__dirname, { shimDir });
+					expect(path.join(shimDir, 'env.bin.bash.sh'))
+						.to.be.a.file('env.bin.bash.sh')
+						.with.content('#!/bin/bash -i\nexport -p');
+				});
+			});
+
+			context('the platform is a unix variant', () => {
+				before(() => {
+					mockPlatform('linux');
+				});
+
+				after(() => {
+					unmockPlatform();
+				});
+
+				it('writes a shim to the file system', () => {
+					loadEnv(__dirname, { shimDir });
+					expect(path.join(shimDir, 'env.bin.sh.sh'))
+						.to.be.a.file('env.bin.sh.sh')
+						.with.content('#!/bin/sh -i\nexport -p');
+				});
+			});
+
+			context('the shell is fish', () => {
+				before(() => {
+					spawnSyncStub.returns(spawnSyncReturnsFactory(loadEnvironmentFixture('fish')));
+					process.env.SHELL = '/usr/local/bin/fish';
+				});
+
+				after(() => {
+					delete process.env.SHELL;
+				});
+
+				it('writes a shim to the file system', () => {
+					loadEnv(__dirname, { shimDir });
+					expect(path.join(shimDir, 'env.usr.local.bin.fish.fish')).to.be.a.file(
+						'env.usr.local.bin.fish.fish'
+					).with.content(`#!/usr/local/bin/fish
+for name in (set -nx)
+	if string match --quiet '*PATH' $name
+		echo $name=(string join : -- $$name)
+	else
+		echo $name="$$name"
+	end
+end`);
+				});
+
+				it('correctly reads the environment', () => {
+					const environment = loadEnv(__dirname, { shimDir });
+					expect(environment).to.deep.eq({
+						GEM_HOME: '/home/someuser/.gem/ruby/2.5.1',
+						GEM_PATH:
+							'/home/someuser/.gem/ruby/2.5.1:/home/someuser/.rubies/ruby-2.5.1/lib/ruby/gems/2.5.0',
+						GEM_ROOT: '/home/someuser/.rubies/ruby-2.5.1/lib/ruby/gems/2.5.0',
+						HOME: '/home/someuser',
+						LANG: 'en_US.UTF-8',
+						RUBYOPT: '',
+						RUBY_ENGINE: 'ruby',
+						RUBY_ROOT: '/home/someuser/.rubies/ruby-2.5.1',
+						RUBY_VERSION: '2.5.1',
+					});
+					expect(environment.SHELL).to.be.undefined;
+				});
+			});
+
+			context('the shell is POSIX compliant', () => {
+				before(() => {
+					spawnSyncStub.returns(spawnSyncReturnsFactory(loadEnvironmentFixture('posix')));
+				});
+
+				it('correctly reads the environment', () => {
+					const environment = loadEnv(__dirname, { shimDir });
+					expect(environment).to.deep.eq({
+						GEM_HOME: '/home/someuser/.gem/ruby/2.5.1',
+						GEM_PATH:
+							'/home/someuser/.gem/ruby/2.5.1:/home/someuser/.rubies/ruby-2.5.1/lib/ruby/gems/2.5.0',
+						GEM_ROOT: '/home/someuser/.rubies/ruby-2.5.1/lib/ruby/gems/2.5.0',
+						HOME: '/home/someuser',
+						LANG: 'en_US.UTF-8',
+						RUBYOPT: '',
+						RUBY_ENGINE: 'ruby',
+						RUBY_ROOT: '/home/someuser/.rubies/ruby-2.5.1',
+						RUBY_VERSION: '2.5.1',
+					});
+					expect(environment.SHELL).to.be.undefined;
+				});
+			});
+		});
+
+		context('a custom path to a shell is used', () => {
+			it('writes a shim with that custom shell path', () => {
+				const shell = '/usr/local/bin/zsh';
+				loadEnv(__dirname, { shell, shimDir });
+				expect(path.join(shimDir, 'env.usr.local.bin.zsh.sh'))
+					.to.be.a.file('env.usr.local.bin.zsh.sh')
+					.with.content(`#!${shell} -i\nexport -p`);
+			});
+		});
+	});
+});
+
+/**
+ * Factory function to build SpawnSyncReturns<Buffer> objects to mock
+ * spawn results
+ *
+ * @param output String representing stdout for stubbed spawn.sync
+ * @param error String representing stderr for stubbed spawn.sync
+ * @returns SpawnSyncReturns object with stdout/stderr buffers
+ */
+function spawnSyncReturnsFactory(output: string, error = ''): SpawnSyncReturns<Buffer> {
+	return {
+		pid: -1,
+		output: [output, error],
+		stdout: Buffer.from(output, 'utf8'),
+		stderr: Buffer.from(error, 'utf8'),
+		status: 0,
+		signal: null,
+	};
+}
+
+/**
+ * Mock process.platform to something
+ *
+ * @param target value to set process.platform to
+ */
+function mockPlatform(target: string): void {
+	Object.defineProperty(process, 'platform', {
+		value: target,
+	});
+}
+
+// Original process.platform
+const platform = process.platform;
+
+/**
+ * Set process.platform back to original value
+ */
+function unmockPlatform(): void {
+	mockPlatform(platform);
+}
+
+/**
+ * Loads a fixture file for a given environment output
+ *
+ * @param variant type of environment to load
+ */
+function loadEnvironmentFixture(variant: string): string {
+	return fs.readFileSync(path.join(fixturesDir, `environment.${variant}.txt`)).toString();
+}
