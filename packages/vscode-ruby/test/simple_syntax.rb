b18c116833c3b3ee31755f5f8252a77fb5dbb39e
Stafford Brunk <wingrunr21@gmail.com>
2020-02-23 17:43:51 -0700

    Add additional meta.function-call.ruby scope matching. Resolves #579

--- packages/vscode-ruby/test/simple_syntax.rb
+++ packages/vscode-ruby/test/simple_syntax.rb
@@ -10,3 +10,24 @@ foo && bar && baz
 # <--- variable.other.ruby
 #      ^^^ variable.other.ruby
 #             ^^^ variable.other.ruby
+
+Klass::Klass2::Klass3.instance.meth
+#                     ^^^^^^^^ meta.function-call.ruby entity.name.function.ruby
+#                              ^^^^ meta.function-call.ruby entity.name.function.ruby
+
+Klass.meth
+#     ^^^^ meta.function-call.ruby entity.name.function.ruby
+Klass.meth()
+#     ^^^^ meta.function-call.ruby entity.name.function.ruby
+Klass.meth 'arg', 'arg', 'arg', :arg, methcall
+#     ^^^^ meta.function-call.ruby entity.name.function.ruby
+Klass.meth('arg', 'arg', 'arg', :arg, methcall)
+#     ^^^^ meta.function-call.ruby entity.name.function.ruby
+
+foo(bar(baz()))
+# <-- meta.function-call.ruby entity.name.function.ruby
+#  ^ punctuation.section.function.ruby
+#   ^^^ meta.function-call.ruby entity.name.function.ruby
+#      ^ punctuation.section.function.ruby
+#       ^^^ meta.function-call.ruby entity.name.function.ruby
+#          ^^^^ punctuation.section.function.ruby
