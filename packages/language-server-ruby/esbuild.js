997ab9ac23920d2be7cbe91de8061a802fff923b
Stafford Brunk <wingrunr21@gmail.com>
2021-05-30 11:25:38 -0600

    chore: strip webpack and implement esbuild. web-tree-sitter-ruby works now

---
+++ packages/language-server-ruby/esbuild.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const outDir = 'dist';
+
+const treeSitterWasmPlugin = {
+  name: 'treeSitterWasm',
+  setup(build) {
+    const wasmPaths = [
+      require.resolve('web-tree-sitter/tree-sitter.wasm'),
+      require.resolve('web-tree-sitter-ruby/tree-sitter-ruby.wasm')
+    ];
+    // build.onEnd(result => {
+    wasmPaths.forEach(wasmPath => {
+      fs.copyFileSync(wasmPath, path.join(outDir, path.basename(wasmPath)))
+    });
+    // })
+  }
+}
+
+require('esbuild').build({
+  entryPoints: ['src/index.ts'],
+  bundle: true,
+  sourcemap: process.env.NODE_ENV !== 'production',
+  minify: true,
+  outfile: `${outDir}/index.js`,
+  external: ['vscode'],
+  format: 'cjs',
+  platform: 'node',
+  plugins: [treeSitterWasmPlugin],
+  watch: !!process.env.ESBUILD_WATCH
+}).catch(() => process.exit(1))
--- packages/language-server-ruby/src/util/TreeSitterFactory.ts
+++ packages/language-server-ruby/src/util/TreeSitterFactory.ts
@@ -1,16 +1,14 @@
-import path from 'path';
 import log from 'loglevel';
 import Parser from 'web-tree-sitter';
-
-const TREE_SITTER_RUBY_WASM = path.resolve(__dirname, 'tree-sitter-ruby.wasm');
+import Ruby from 'web-tree-sitter-ruby';
 
 const TreeSitterFactory = {
 	language: null,
 
 	async initalize(): Promise<void> {
 		await Parser.init();
-		log.debug(`Loading Ruby tree-sitter syntax from ${TREE_SITTER_RUBY_WASM}`);
-		this.language = await Parser.Language.load(TREE_SITTER_RUBY_WASM);
+		log.debug(`Loading Ruby tree-sitter syntax from ${Ruby}`);
+		this.language = await Parser.Language.load(Ruby);
 	},
 
 	build(): Parser {
